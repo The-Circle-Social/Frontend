f50af7d92b5f9e46d165e4fc3228bd4d0421c1a1
Heet Vakharia <heet1476@gmail.com>
2021-01-30 18:07:58 +0530

    Added Update Profile

--- src/components/UserProfile.component.jsx
+++ src/components/UserProfile.component.jsx
@@ -8,10 +8,20 @@ const UserProfile = () => {
         axios({
             url:"https://randomuser.me/api/",
             method:"GET"
-        }).then(console.log)
-    })
+        }).then(({data}) =>{
+            setUserInfo(data.results[0])
+        })
+    },[])
     return ( 
-        <div className="">User Profile</div>
+        <div className="">
+          {userinfo.picture && userinfo.name?
+             <>
+                <img src={userinfo.picture.medium || ""} alt="User"/>
+                <h1>{`${userinfo.name.title} ${userinfo.name.first} ${userinfo.name.last}`}</h1>
+                <p>Quaerat itaque dolorem. Rerum odio voluptatem. Dolores voluptas laborum non. Ut ipsam consequatur omnis. Perspiciatis est asperiores voluptatem voluptas sint aut. Laborum et et blanditiis quas ut est sit.</p>
+            </>
+           :null}
+        </div>
      );
 }
  
